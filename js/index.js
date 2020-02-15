bd96259e37b958488679c62b49bb9817b7f829e9
Luan vu <luanvu1711@gmail.com>
2020-02-14 20:56:31 -0500

    finished 10/10

--- js/index.js
+++ js/index.js
@@ -27,3 +27,54 @@ let imgDestination = document.querySelector('#img-destination');
 imgDestination.addEventListener('dblclick', () => {
         imgDestination.style.display = "none";   
 })
+imgDestination.addEventListener('dragstart', () => {
+    alert('Please stop doing that!');
+})
+
+let container = document.querySelector('.container');
+container.addEventListener('mousedown', () => {
+    container.style.background = "#b3d1ff";
+})
+container.addEventListener('mouseup', () => {
+    container.style.background = 'white';
+})
+
+let imgIntro = document.querySelector('.intro img');
+imgIntro.addEventListener('contextmenu', () => {
+    imgIntro.style.transform = "scale(0.8)";
+    imgIntro.style.transition = "all 0.4s";
+})
+imgIntro.addEventListener('drag', () => {
+    imgIntro.style.display = 'none';
+})
+
+let pIntro = document.querySelector('.intro p');
+window.addEventListener('scroll', () => {
+    if(scrollY >= 120){
+        pIntro.style.display = 'none';
+    } else {
+        pIntro.style.display = 'block';
+    }
+})
+
+
+
+let home = document.querySelector('.home');
+home.addEventListener('click', (e) => {
+    home.style.background = '#ffb3d9';
+})
+
+
+let textContent = document.querySelector('.text-content');
+textContent.addEventListener('click', (e) => {
+    textContent.style.background = '#ccfff2';
+    e.stopPropagation();
+})
+
+let a = document.querySelectorAll('a');
+a.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+    })
+})
+
